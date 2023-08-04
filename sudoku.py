c052fc32a4e587e8ea121a45b46b912ce24dc1b1
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-08-04 02:14:18 +0100

    changed class to accept any chars as cells

--- sudoku.py
+++ sudoku.py
@@ -1,27 +1,27 @@
 class Sudoku(object):
     def __init__(self, grid_size=9, square_size=3) -> None:
         self.grid_size = grid_size
-        self.grid = [[0 for i in range(grid_size)] for i in range(grid_size)]
+        self.grid = [["0" for i in range(grid_size)] for i in range(grid_size)]
         self.square_size = square_size
 
-    def isValid(self, board):
-        squares = [[] for i in range(len(board) // self.square_size)]
-        for i in range(len(board)):
+    def isValid(self):
+        squares = [[] for i in range(self.grid_size // self.square_size)]
+        for i in range(self.grid_size):
             row = []
             col = []
-            for j in range(len(board[i])):
-                if board[i][j] != 0:
-                    row += board[i][j]
-                if board[j][i] != 0:
-                    col += board[j][i]
-                if board[i][j] != 0:
-                    squares[j // self.square_size] += board[i][j]
+            for j in range(self.grid_size):
+                if self.grid[i][j] != "0":
+                    row += self.grid[i][j]
+                if self.grid[j][i] != "0":
+                    col += self.grid[j][i]
+                if self.grid[i][j] != "0":
+                    squares[j // self.square_size] += self.grid[i][j]
 
             if (i + 1) % self.square_size == 0:
                 for square in squares:
                     if len(set(square)) != len(square):
                         return False
-                squares = [[] for i in range(len(board) // self.square_size)]
+                squares = [[] for i in range(self.grid_size // self.square_size)]
             if len(set(row)) != len(row):
                 return False
             if len(set(col)) != len(col):
