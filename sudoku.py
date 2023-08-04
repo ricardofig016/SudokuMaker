6a8a26865b62616ceb6690f01bce4d823a0f69f5
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-08-04 14:54:20 +0100

    isLegalMove method

--- sudoku.py
+++ sudoku.py
@@ -39,11 +39,45 @@ class Sudoku(object):
 
         return True
 
+    def isLegalMove(self, cell_x, cell_y):
+        cell = (cell_x - 1, cell_y - 1)
+        cell_square = (cell[0] // self.square_size, cell[1] // self.square_size)
+        row = []
+        col = []
+        square = []
+        for i in range(self.grid_size):
+            if self.grid[cell[0]][i] != "0":
+                row.append(self.grid[cell[0]][i])
+            if self.grid[i][cell[1]] != "0":
+                col.append(self.grid[i][cell[1]])
+            if i // self.square_size == cell_square[0]:
+                for j in range(self.grid_size):
+                    if (
+                        j // self.square_size == cell_square[1]
+                        and self.grid[i][j] != "0"
+                    ):
+                        square.append(self.grid[i][j])
+        print(row, col, square)
+        if (
+            len(row) == len(set(row))
+            and len(col) == len(set(col))
+            and len(square) == len(set(square))
+        ):
+            return True
+        return False
+
     def isCompleted(self):
         for row in self.grid:
             if "0" in row:
                 return False
         return True
+
+    def solve(self):
+        ptr = (0, 0)
+        while ptr != (self.grid_size + 1, 0):
+            pass
+
+        return
 
     def toString(self):
         s = ""
