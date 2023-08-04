ea1d6e7415acfd4a01848c06392076b8e356d892
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-08-04 02:08:50 +0100

    added function read_grid in main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from sudoku import Sudoku
+
+
+def read_grid(file_path):
+    with open(file_path, "r") as file:
+        grid = []
+        for line in file:
+            line = line.strip()
+            if line:
+                row = [int(i) for i in line]
+                grid.append(row)
+        return grid
+
+
+if __name__ == "__main__":
+    file_path = "dummy_grids/grid1.txt"
+    sudoku = Sudoku()
+    sudoku.grid = read_grid(file_path)
+    print(sudoku.grid)
