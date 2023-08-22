eb2916cf5fbc13fd2860388e8c7a5db420094859
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-08-22 01:38:33 +0100

    turtle rendering of random grid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sudoku import Sudoku
+import turtle
 
 
 def read_grid(file_path):
@@ -12,9 +13,63 @@ def read_grid(file_path):
         return grid
 
 
+def text(message, x, y, size, align):
+    FONT = ("Arial", size, "normal")
+    pen.penup()
+    pen.goto(x, y)
+    pen.write(message, align=align, font=FONT)
+
+
+def drawGrid(grid):
+    pen.clear()
+    intDim = 35
+    for row in range(0, 10):
+        if (row % 3) == 0:
+            pen.pensize(3)
+        else:
+            pen.pensize(1)
+        pen.penup()
+        pen.goto(topLeft_x, topLeft_y - row * intDim)
+        pen.pendown()
+        pen.goto(topLeft_x + 9 * intDim, topLeft_y - row * intDim)
+    for col in range(0, 10):
+        if (col % 3) == 0:
+            pen.pensize(3)
+        else:
+            pen.pensize(1)
+        pen.penup()
+        pen.goto(topLeft_x + col * intDim, topLeft_y)
+        pen.pendown()
+        pen.goto(topLeft_x + col * intDim, topLeft_y - 9 * intDim)
+
+    for row in range(0, 9):
+        for col in range(0, 9):
+            if grid[row][col] != "0":
+                text(
+                    grid[row][col],
+                    topLeft_x + col * intDim + 9,
+                    topLeft_y - row * intDim - intDim + 8,
+                    18,
+                    "left",
+                )
+
+
 if __name__ == "__main__":
-    # grid_file_nb = input("grid file number: ")
-    # file_path = "dummy_grids/grid" + grid_file_nb + ".txt"
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color("#000000")
+    pen.hideturtle()
+    topLeft_x = -150
+    topLeft_y = 150
+
+    text("loading grid", 0, 0, 18, "center")
+
+    # file_path = "dummy_grids/grid1.txt"
     # sud = Sudoku(grid=read_grid(file_path))
+
     sud = Sudoku(9, 3)
-    print(sud.genRandomGrid().toString())
+    sud = sud.genRandomGrid()
+
+    drawGrid(sud.grid)
+
+    turtle.done()
